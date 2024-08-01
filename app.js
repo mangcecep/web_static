e84e118b605e10654a9aa647ba645ad9d2cab94d
Cecep <cecepsolihinyusup@gmail.com>
2024-08-01 08:15:09 +0700

    modifikasi fungsi tambah

--- app.js
+++ app.js
@@ -1,41 +1,21 @@
-const buttonSubmit = document.getElementById('submit')
+const input = document.getElementById('to_do_list_input')
+const button = document.getElementById('submit')
 const toDoList = document.getElementById('to_do_list')
-const toDoListInput = document.getElementById('to_do_list_input')
-const deleteButton = document.getElementById('text-delete')
 
-let toDoListData = []
+let taskList = [];
 
-function deleteFunction(idx) {
-    toDoListData = toDoListData.filter((_, id) => id != idx)
-    let newData = toDoListData.filter((_, c) => c !== idx).map((res, idx) => {
-        return `<li>${res} <a href="#" class="text-danger ml-4" onclick="deleteFunction('${idx}')">x</a></li>`
-    }).join('')
-    toDoList.innerHTML = newData
-}
+button.addEventListener('click', function () {
+    let inputValue = input.value
+    // inputValue === '' ? alert('tidak boleh kosong') : console.log(`${inputValue}`)
 
-buttonSubmit.addEventListener('click', function () {
-
-    console.log(`VALUE INPUT ${toDoListInput.value}`)
-
-    if (toDoListInput.value === '') {
-        alert(`Input tidak boleh kosong!`)
+    if (inputValue === '') {
+        alert('tidak boleh kosong')
         return
     }
 
-    toDoListData?.push(toDoListInput.value)
-    let data = toDoListData?.map((res, idx) => {
-        return `<li>${res} <a href="#" class="text-danger ml-4" onclick="deleteFunction('${idx}')">x</a></li>`
-    }).join('')
-
-    console.log(data)
-
-    toDoList.innerHTML = `${data}`
-})
-
-
-
-window.addEventListener('load', function () {
-    if (toDoListData?.length === 0) {
-        toDoList.innerHTML = '<li>Belum Ada data TO Do List</li>'
-    }
+    taskList.push(inputValue)
+    // console.log(taskList)
+    let newVal = taskList.map(v => `<li>${v}</li>`).join('')
+    // console.log(newVal)
+    toDoList.innerHTML = newVal
 })
