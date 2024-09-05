590b9ad644a07dc6ee649c7936a050510e87050b
Cecep <cecepsolihinyusup@gmail.com>
2024-09-05 13:55:01 +0700

    delete function

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const input = document.getElementById('to_do_list_input')
-const button = document.getElementById('submit')
-const toDoList = document.getElementById('to_do_list')
+const button = document.getElementById("submit")
+const input = document.getElementById("to_do_list_input");
+const todoList = document.getElementById("to_do_list")
 
-let taskList = [];
+let data = [];
+let id = 0;
 
-button.addEventListener('click', function () {
-    let inputValue = input.value
-    // inputValue === '' ? alert('tidak boleh kosong') : console.log(`${inputValue}`)
+button.addEventListener("click", function () {
 
-    if (inputValue === '') {
-        alert('tidak boleh kosong')
+    let valueInput = input.value
+
+    if (valueInput === '') {
+        alert("Input tidak boleh kosong!")
         return
     }
 
-    taskList.push(inputValue)
-    // console.log(taskList)
-    let newVal = taskList.map(v => `<li>${v}</li>`).join('')
-    // console.log(newVal)
-    toDoList.innerHTML = newVal
+    data.push({
+        id: id++,
+        value: valueInput
+    });
+
+    console.log(data)
+
+    let newData = data.map(d => `<li>${d.value} <a href="#" class="text-danger" onclick="hapusData(${d.id})">x</a></li>`).join("")
+    todoList.innerHTML = newData
+    console.log(`value input ${newData}`)
 })
+
+function hapusData(id) {
+    let newData = data.filter(value => value.id != id)
+    data = newData
+    todoList.innerHTML = data.map(d => `<li>${d.value} <a href="#" class="text-danger" onclick="hapusData(${d.id})">x</a></li>`).join("")
+}
